407a3e9c37ad7851fcb342b16fc498942089d081
Nick <haronicolas90@gmail.com>
2026-02-26 08:21:24 -0500

    agrege par e impar

---
+++ 02_par_impar.py
@@ -0,0 +1,10 @@
+numero=int(input("Mete un numero mi so"))
+if numero % 2 == 0:
+    print ("Es par")
+else:
+    print ("Es impar")
+edad= int(input("Recuerdame tu edad "))
+if edad>=18:
+    print("Es mayor ponganlo a chambear")
+else:
+    print("Es menor cuidenlo de Epstein")
