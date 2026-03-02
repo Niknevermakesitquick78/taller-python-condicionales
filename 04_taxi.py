9eddbe57782e8ae2afe2974cfa81e699e9ce0db6
Niknevermakesitquick78 <nharoc@estud.usfq.edu.ec>
2026-03-02 14:44:56 -0500

    Fix input prompts and improve cost calculation

--- 04_taxi.py
+++ 04_taxi.py
@@ -1,16 +1,18 @@
-distancia= float(input("Ingrese la distancia recorrdia en km:"))
-hora = int(input("Ingrese la hora del viaje (0-23):"))
+distancia = float(input("Ingrese la distancia recorrida en km: "))
+hora = int(input("Ingrese la hora del viaje (0-23): "))
 total = 1.0
-if 6<= hora <= 19:
-    tipo_horario= "diruno"
-    costo_km= 0.65
+if 6 <= hora <= 19:
+    tipo_horario = "diurno"
+    costo_km = 0.50
+elif 0 <= hora <= 23:
+    tipo_horario = "nocturno"
+    costo_km = 0.65
 else:
-    print("Hora invalida mi so")
-    costo_km = 0
-    tipo_horario = "desconocido"
-total += distancia* costo_km
-if distancia>10:
+    print("Hora inválida")
+    exit()
+total += distancia * costo_km
+if distancia > 10:
     total += 2
-print("Horario", tipo_horario)
-print("Distancia recorrida", distancia, "km")
-print("Debes pagar en total", round(total, 2))
+print("Horario:", tipo_horario)
+print("Distancia:", distancia, "km")
+print("Total a pagar: $", round(total, 2))
