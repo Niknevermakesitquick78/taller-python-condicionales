a81389fd3922fb903858197edf8f02c632fe3486
Niknevermakesitquick78 <nharoc@estud.usfq.edu.ec>
2026-03-02 14:51:57 -0500

    Fix typo and improve customer type validation

--- 03_descuento.py
+++ 03_descuento.py
@@ -1,17 +1,15 @@
-subtotal = float(input("Ingresa un subtotal guapo:"))
-tipo_cliente_155= input("Ingrese el tipo de clinte que es (vip o regular):")
-descuento= 0
-if tipo_cliente_155 == "vip":
-    descuento= subtotal*0.15
-elif tipo_cliente_155== "regular":
-    if subtotal>=100:
-        descuento= subtotal*0.05
-    else:
-        descuento= 0
+subtotal = float(input("Ingresa un subtotal guapo: "))
+tipo_cliente = input("Ingrese el tipo de cliente que es (vip o regular): ")
+descuento = 0
+if tipo_cliente == "vip":
+    descuento = subtotal * 0.15
+elif tipo_cliente == "regular":
+    if subtotal >= 100:
+        descuento = subtotal * 0.05
 else:
-    print("Tipo de cliente no valido")
-    desuento= 0
+    print("Tipo de cliente no válido")
+    descuento = 0
 total_final = subtotal - descuento
-print("subtotal:", subtotal)
+print("Subtotal:", subtotal)
 print("Descuento aplicado:", descuento)
-print ("Total final:", total_final)
+print("Total final:", total_final)
